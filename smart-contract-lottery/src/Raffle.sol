cafea23ed2d923e32c4abe8a016cd696175e76df
Dmytro Shashkevych <shashkevich41@gmail.com>
2024-07-31 14:10:37 +0300

    added events

--- smart-contract-lottery/src/Raffle.sol
+++ smart-contract-lottery/src/Raffle.sol
@@ -1,3 +1,24 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
@@ -9,13 +30,24 @@ pragma solidity ^0.8.18;
  * @dev Implements Chainling VRFv2
  */
 contract Raffle {
+    error Raffle__NotEnoughEntrenceFee();
+
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    event EnteredRaffle(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() external payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEntrenceFee();
+        }
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
+    }
 
     function pickWinner() public {}
 
