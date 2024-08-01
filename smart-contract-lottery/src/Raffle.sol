9e83204a413a001636694b6074bc4aae85e7b2f3
Dmytro Shashkevych <shashkevich41@gmail.com>
2024-08-01 17:10:13 +0300

    deployed and added chainlinkVRF subscription contract

--- smart-contract-lottery/src/Raffle.sol
+++ smart-contract-lottery/src/Raffle.sol
@@ -31,14 +31,25 @@ pragma solidity ^0.8.18;
  */
 contract Raffle {
     error Raffle__NotEnoughEntrenceFee();
+    error Raffle__NotEnoughTimeToPickWinner();
 
     uint256 private immutable i_entranceFee;
-    address payable[] private s_players;
+    // @dev Duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    address private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+
+    address[] private s_players;
+    uint256 private s_lastTimeStemp;
 
     event EnteredRaffle(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(uint256 entranceFee, uint256 interval, address vrfCoordinator, bytes32 gasLane) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        i_vrfCoordinator = vrfCoordinator;
+        i_gasLane = gasLane;
+        s_lastTimeStemp = block.timestamp;
     }
 
     function enterRaffle() external payable {
@@ -49,7 +60,18 @@ contract Raffle {
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() public {}
+    function pickWinner() external {
+        // if (block.timestamp - s_lastTimeStemp < i_interval) {
+        //     revert Raffle__NotEnoughTimeToPickWinner();
+        // }
+        //         unit256 requestId = i_vrfCoordinator.requestRandomWords(
+        //     i_gasLane,
+        //     uint64 subId,
+        //     uint16 requestConfirmations,
+        //     uint32 callbackGasLimit,
+        //     uint32 numWords
+        //   )
+    }
 
     function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
